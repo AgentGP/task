ad98d74875e2112f6a65b1c60425c22f2a8fffd7
AgentGP <aleks.belan05@gmail.com>
2025-02-12 01:05:25 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const tasksRoutes = require('./routes/tasksRoutes')
 // This file is responsible for the server itself (listening and connecting the logic frontend <=> tasks.json)
 
 const app = express()
+app.use(express.json())
 app.use("/tasks", tasksRoutes)
 
 const PORT = process.env.PORT || 5000
